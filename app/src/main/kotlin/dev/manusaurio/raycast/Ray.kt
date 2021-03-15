8efdb4c313a1bc66a2f1ce623aab7abd5b3d5cfa
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2021-03-15 16:55:15 -0300

    Fix and finish function to find line intersections
    As it is now, this code makes it possible to implement an algorithm to find nearby points by making rays and checking if they intersect with every existing wall, needing m*n operations.
    
    That's fine, albeit I'd like to try the digital differential analyzer algorithm as well, so that'll be the next thing to implement.

--- app/src/main/kotlin/dev/manusaurio/raycast/Ray.kt
+++ app/src/main/kotlin/dev/manusaurio/raycast/Ray.kt
@@ -1,53 +1,64 @@
 package dev.manusaurio.raycast
 
 class Ray private constructor(
-    val x: Double,
-    val y: Double,
-    val tX: Double,
-    val tY: Double,
-    private val point: Point = Point(x, y)
+    val position: Point,
+    val target: Point,
 ) {
-    constructor(x: Double, y: Double, targetX: Double, targetY: Double)
-            : this(x, y, targetX, targetY, Point(x.toDouble(), y.toDouble()))
+    val direction: Point = Point(target.x - position.x, target.y - position.y)
 
-    fun magnitude() = point.magnitude()
+    companion object {
+        operator fun invoke(position: Point, target: Point): Ray {
+            return Ray(
+                position,
+                target,
+            )
+        }
 
-    private fun newPoint(targetPoint: Point) = with(targetPoint) {
-        Ray(x, y, tX, tY, this)
+        operator fun invoke(positionX: Double, positionY: Double, targetX: Double, targetY: Double): Ray {
+            return Ray(
+                Point(positionX, positionY),
+                Point(targetX, targetY),
+            )
+        }
     }
 
-    operator fun plus(other: Ray) = newPoint(point + other.point)
+    // TODO: raycasting functions
 
-    operator fun minus(other: Ray) = newPoint(point - other.point)
+    fun magnitude() = position.magnitude()
 
-    operator fun times(n: Double) = newPoint(point * n)
+    operator fun component1() = position.x
 
-    operator fun div(n: Double) = newPoint(point / n)
+    operator fun component2() = position.y
 
-    fun findIntersection(ray: Ray, line: Line): Point? {
+    operator fun component3() = target.x
+
+    operator fun component4() = target.y
+
+    fun findIntersection(line: Line): Point? {
+        // based on Daniel Shiffman's line-line intersection finding implementation
         val x0 = line.a.x
         val y0 = line.a.y
         val x1 = line.b.x
         val y1 = line.b.y
 
-        val x2 = ray.x
-        val y2 = ray.y
-        val x3 = ray.x + ray.tY
-        val y3 = ray.y + ray.tX
+        val x2 = position.x
+        val y2 = position.y
+        val x3 = position.x + direction.x
+        val y3 = position.y + direction.y
 
         return when (val den = (x0 - x1) * (y2  - y3) - (y0 - y1) * (x2 - x3)) {
             0.0 -> null
             else -> {
-                val t = ((x0 - x3) * (y2 - y3) - (y0 - y2) * (x2 - x3)) / den
-                val u = ((x0 - x1) * (y0 - y2) - (y0 - y1) * (x0 - x2)) / den
+                val t = ((x0 - x2) * (y2 - y3) - (y0 - y2) * (x2 - x3)) / den
+                val u = -((x0 - x1) * (y0 - y2) - (y0 - y1) * (x0 - x2)) / den
 
                 if (t > 0.0 && t < 1.0 && u > 0.0) {
                     Point(
                         x0 + t * (x1 - x0),
-                        y0 - t * (y1 - y0)
+                        y0 + t * (y1 - y0)
                     )
                 }
-                null
+                else null
             }
         }
     }
